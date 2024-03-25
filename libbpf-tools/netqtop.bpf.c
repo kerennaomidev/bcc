76faeb0266c07998b90f44cc799d871aa4280863
Keren Naomi <Keren.Devnesh@sony.com>
2024-03-26 01:49:24 +0530

    cleared all errors and warnings but there's an object creation error, looking into it

--- libbpf-tools/netqtop.bpf.c
+++ libbpf-tools/netqtop.bpf.c
@@ -1,50 +1,83 @@
 #include <vmlinux.h>
-#include <bpf/bpf_helpers.h>
 #include <bpf/bpf_core_read.h>
 #include <bpf/bpf_tracing.h>
 #include "netqtop.h"
-#include "core_fixes.bpf.h"
+#include <bpf/bpf_helpers.h>
 
-const volatile pid_t targ_pid = 0;
-const volatile pid_t targ_tgid = 0;
-
+// Define BPF map types
 struct {
-	__uint(type, BPF_ARRAY);
-	__uint(max_entries, 1);
-	__type(key, int);
-	__type(value, union name_buf);
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+    __uint(max_entries, 1);
+    __type(key, int);
+    __type(value, union name_buf);
 } name_map SEC(".maps");
 
 struct {
-	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, 1024);
-	__type(key, u16);
-	__type(value, struct queue_data);
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 1024);
+    __type(key, u16);
+    __type(value, struct queue_data);
 } txevent SEC(".maps");
 
 struct {
-	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, 1024);
-	__type(key, u16);
-	__type(value, struct queue_data);
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __uint(max_entries, 1024);
+    __type(key, u16);
+    __type(value, struct queue_data);
 } rxevent SEC(".maps");
+
+static inline int name_filter(const char *dev_name) {
+    union name_buf real_devname;
+    bpf_probe_read_kernel(&real_devname, sizeof(real_devname), dev_name);
+
+    int key = 0;
+    union name_buf *leaf = bpf_map_lookup_elem(&name_map, &key);
+    if (!leaf) {
+        return 0;
+    }
+    if ((leaf->name_int).hi != real_devname.name_int.hi || (leaf->name_int).lo != real_devname.name_int.lo) {
+        return 0;
+    }
+
+    return 1;
+}
+
+static void update_data(struct queue_data *data, u64 len) {
+    data->total_pkt_len += len;
+    data->num_pkt++;
+
+    if (len < 64) {
+        data->size_64B++;
+    } else if (len < 512) {
+        data->size_512B++;
+    } else if (len < 2048) {
+        data->size_2K++;
+    } else if (len < 16384) {
+        data->size_16K++;
+    } else {
+        data->size_64K++;
+    }
+}
 
 SEC("tracepoint/net/net_dev_start_xmit")
 int trace_net_dev_start_xmit(struct trace_event_raw_sys_enter *ctx) {
-    struct sk_buff *skb = (struct sk_buff*)ctx->args[0];
-    if (!name_filter(skb)) {
-        return 0;
-    }
-    u16 qid = skb->queue_mapping;
-    struct queue_data *data = tx_q.lookup(&qid);
-    if (!data) {
-        struct queue_data newdata = {0};  
-        bpf_map_update_elem(&txevent, &qid, &newdata, BPF_ANY);
+    struct sk_buff skb;
+    bpf_probe_read_kernel(&skb, sizeof(skb), (void *)ctx->args[0]);
+
+    if (!name_filter(skb.dev->name)) {
         return 0;
     }
 
-    update_data(data, skb->len);
-    bpf_map_update_elem(&txevent, &qid, data, BPF_ANY);
+    u16 qid = skb.queue_mapping;
+    struct queue_data *data = bpf_map_lookup_elem(&txevent, &qid);
+    if (!data) {
+        struct queue_data newdata = {0};
+        bpf_map_update_elem(&txevent, &qid, &newdata, BPF_NOEXIST);
+        return 0;
+    }
+
+    update_data(data, skb.len);
+    bpf_map_update_elem(&txevent, &qid, data, 0);
 
     return 0;
 }
@@ -52,37 +85,28 @@ int trace_net_dev_start_xmit(struct trace_event_raw_sys_enter *ctx) {
 SEC("tracepoint/net/netif_receive_skb")
 int trace_netif_receive_skb(struct trace_event_raw_sys_enter *ctx) {
     struct sk_buff skb;
+    bpf_probe_read_kernel(&skb, sizeof(skb), (void *)ctx->args[0]);
 
-    bpf_probe_read(&skb, sizeof(skb), ctx->args[0]);
-    
-    if (!name_filter(&skb)) {
-        return 0;  
+    if (!name_filter(skb.dev->name)) {
+        return 0;
     }
 
     u16 qid = 0;
-    if (skb_rx_queue_recorded(&skb)) {
-        qid = skb_get_rx_queue(&skb);
-    }
+    // Since bpf_skb_rx_queue_recorded and bpf_skb_get_rx_queue are not available,
+    // we'll omit the queue-related functionality here.
 
-    struct queue_data newdata;
-    __builtin_memset(&newdata, 0, sizeof(newdata));
-    struct queue_data *data = rx_q.lookup_or_try_init(&qid, &newdata);
+    struct queue_data *data = bpf_map_lookup_elem(&rxevent, &qid);
     if (!data) {
-        struct queue_data newdata = {0};  
-        bpf_map_update_elem(&txevent, &qid, &newdata, BPF_ANY);
-        return 0;  
+        struct queue_data newdata = {0};
+        bpf_map_update_elem(&rxevent, &qid, &newdata, BPF_NOEXIST);
+        return 0;
     }
 
     update_data(data, skb.len);
-    bpf_map_update_elem(&rxevent, &qid, data, BPF_ANY);
+    bpf_map_update_elem(&rxevent, &qid, data, 0);
 
-    return 0; 
+    return 0;
 }
 
-
-//cleanup:
-//	bpf_map_delete_elem(&name_map);
-//	return 0;
-//}
-
 char LICENSE[] SEC("license") = "GPL";
+
