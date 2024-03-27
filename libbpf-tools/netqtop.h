0bddb875dd7715d42edc3b68fc8aae7f8c572a60
Keren Naomi <Keren.Devnesh@sony.com>
2024-03-27 14:18:24 +0530

    __u

--- libbpf-tools/netqtop.h
+++ libbpf-tools/netqtop.h
@@ -7,21 +7,17 @@
 union name_buf{
     char name[IFNAMSIZ];
     struct {
-        u64 hi;
-        u64 lo;
+        __u64 hi;
+        __u64 lo;
     }name_int;
 };
 struct queue_data{
-    u64 total_pkt_len;
-    u32 num_pkt;
-    u32 size_64B;
-    u32 size_512B;
-    u32 size_2K;
-    u32 size_16K;
-    u32 size_64K;
+    __u64 total_pkt_len;
+    __u32 num_pkt;
+    __u32 size_64B;
+    __u32 size_512B;
+    __u32 size_2K;
+    __u32 size_16K;
+    __u32 size_64K;
 };
-
-
-
-
 #endif
